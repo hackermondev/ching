fd320731cba68cd6028c75ee818e2668c62efd9f
Human <therealhackermon@gmail.com>
2021-03-07 15:57:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var WebTorrent = require('webtorrent')
+
+var client = new WebTorrent()
+var magnetURI = 'magnet:?xt=urn:btih:A11264196083EF44C813550F611C8BAC77CC45FC&dn=The.Flash.2014.S07E01.HDTV.x264-PHOENiX%5BTGx%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce'
+
+client.add(magnetURI, function (torrent) {
+  // Got torrent metadata!
+  console.log('Client is downloading:', torrent.infoHash)
+
+  torrent.files.forEach(function (file) {
+    console.log(file)
+  })
+})
